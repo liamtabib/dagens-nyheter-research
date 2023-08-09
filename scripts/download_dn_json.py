3020eb9c712d62692399b1cefb7a51a72e8e6c4a
liamtabib <liam.tab7@gmail.com>
2023-08-09 10:06:26 +0200

    block and textline alignment

--- scripts/download_dn_json.py
+++ scripts/download_dn_json.py
@@ -6,7 +6,6 @@ import multiprocessing
 import json
 import argparse
 import os
-import xml.etree.ElementTree as ET
 
 def get_ids(pw,n_editions):
     """returns all IDs of the matches in the betalab API for the 'Dagens nyheter' search'"""
@@ -16,15 +15,23 @@ def get_ids(pw,n_editions):
         ids.append(package_id)
     return ids
 
-
 def store_files(package_id, pw, p):
     """ Reads an id of a specific edition and stores content files, metadata files, and structure files.
     """
-    a = Archive("https://betalab.kb.se", auth=("demo", pw))
-
     try:
         meta = requests.get(f"https://betalab.kb.se/{package_id}/meta.json", auth=HTTPBasicAuth("demo", pw))
         meta = json.loads(meta.text)
+
+        content = requests.get(f"https://betalab.kb.se/{package_id}/content.json", auth=HTTPBasicAuth("demo", pw))
+        content = json.loads(content.text)
+
+        stucture = requests.get(f"https://betalab.kb.se/{package_id}/structure.json", auth=HTTPBasicAuth("demo", pw))
+        structure = json.loads(stucture.text)
+
+        file_name_meta = package_id + '_meta.json'
+        file_name_content = package_id + '_content.json'
+        file_name_structure = package_id + '_structure.json'
+
         year=meta['year']
 
         year_path= p / year
@@ -39,42 +46,46 @@ def store_files(package_id, pw, p):
             pass
         else:
             os.makedirs(edition_path)
-            
-        for x in a.get(package_id):
-                
-                if "alto.xml" in x:
-                    page=requests.get(f"https://betalab.kb.se/{package_id}/{x}", auth=HTTPBasicAuth("demo", pw),stream=True)
-                    if page.status_code == 200:
-                        tree= ET.ElementTree(ET.fromstring(page.text))
-                        page_name = x.split("_")
-                        page_number = page_name[-2]
-                        file_name_xml = package_id + f'_{page_number}.xml'
-                        filepath_xml= edition_path / file_name_xml
-                        #Download the file
+        
+        filepath_meta = edition_path / file_name_meta
+        filepath_content = edition_path / file_name_content
+        filepath_structure = edition_path / file_name_structure
 
-                        tree.write(filepath_xml)
+        with filepath_meta.open("w", encoding="utf-8") as f:
+            json.dump(meta, f, indent=2)
+
+        with filepath_content.open("w", encoding="utf-8") as f:
+            json.dump(content, f, indent=2)
+
+        with filepath_structure.open("w", encoding="utf-8") as f:
+            json.dump(structure, f, indent=2)
 
     except Exception as e:
         print(f"Error occurred for package ID: {package_id} - {str(e)}")
 
-
 def count_files():
-    years=list(Path('corpus/raw_xml/').glob('*'))
-    n_files_=0
+    n_files=len(list(Path('corpus/json_Dagens_nyheter/').rglob('*.json')))
+    n_structure_files=len(list(Path('corpus/json_Dagens_nyheter/').rglob('*structure.json')))
+    n_content_files=len(list(Path('corpus/json_Dagens_nyheter/').rglob('*content.json')))
+    n_meta_files=len(list(Path('corpus/json_Dagens_nyheter/').rglob('*meta.json')))
+    print(f'there are {n_files} raw json files in total, of which {n_content_files} are content type,\
+    {n_structure_files} are structure type, and {n_meta_files} are metadata type')
+    years=Path('corpus/json_Dagens_nyheter/').glob('*')
+
     d={}
     for year in years:
-        n_editions=len(list(Path(year).glob('*')))
-        n_files_+=n_editions
-        d[int(year.name)]=n_editions
-    for year, n_files in sorted(d.items()):
+        n_structure_files=len(list(Path(year).rglob('*structure.json')))
+        n_content_files=len(list(Path(year).rglob('*content.json')))
+        n_meta_files=len(list(Path(year).rglob('*meta.json')))
+        assert n_structure_files == n_content_files == n_meta_files
+        d[int(year.name)]=n_content_files
+    for year , n_files in sorted(d.items()):
         print(f'{year} has {n_files} editions')
-    
-    print(f'there are {n_files_} editions inside raw_xml directory')
 
 
 
 def main(args):
-    p = Path("corpus/raw_xml/")
+    p = Path("corpus/json_Dagens_nyheter/")
     p.mkdir(parents=True, exist_ok=True)
     with open(args.path_kb_cred, 'r') as file:
         pw = file.read().replace('\n', '')
